a67a521b380dbff70089df3b145bcdbbf5bf4459
Nicholas Bianco <nbianco@stanford.edu>
2024-03-20 10:47:01 -0700

    Update the README.md

--- README.md
+++ README.md
@@ -1,6 +1,97 @@
 # balance-exo-sim
 
-NOTE: In a previous version of this project, we refered to ankle exoskeleton
+Simulations to understand the effect of ankle exoskeleton torques on changes 
+in center of mass kinematics during walking.
+
+Models, data, and results from this project can be found on SimTK.org: 
+- https://simtk.org/projects/balance-exo-sim
+
+## Publication
+
+Bianco, N. A., Collins, S. H., Liu, K., and Delp, S. L., “Simulating the effect 
+of ankle plantarflexion and inversion-eversion exoskeleton torques on center of 
+mass kinematics during walking,” PLOS Comp Bio, 2023. 
+doi: 10.1371/journal.pcbi.1010712. (2023)
+
+Software requirements
+---------------------
+- OpenSim 4.4.1 or later
+- R 4.2.1 or later (if running the statistics)
+- Python 3.8 (an Anaconda environment can be loaded from conda_enviroment.yml)
+  - pandas
+  - pyyaml==5.4.1
+  - doit
+  - opencv
+  - matplotlib
+  - scipy
+  - numpy=1.20.2
+
+Config file setup
+-----------------
+Before running any simulations, you will need create a file called "config.yml"
+and place it in the top directory of the repository. This file will contain
+various values and paths needed to run the simulation pipeline. 
+
+The following four entries are used to configure certain aspects of the 
+simulation pipeline to be include or exclude. The following settings skip the 
+trajectory optimization tasks (solutions are already included in the 
+repository) but run the time-stepping integration tasks that produce model 
+kinematic changes in response to applied exoskeleton torques:
+
+unperturbed_initial_guess: True
+skip_tracking_solve: True
+skip_timestepping_solve: False
+enable_perturbed_plotting_tasks: False
+
+The remaining entries are paths that will be specific to your system and 
+represent the top-level repository path need by the 'doit' Python package, the
+OpenSim installation directory, the directory containing the raw mocap data, and
+subdirectories needed for different parts of the simulation pipeline:
+
+doit_path: Path to the repository root 
+           (i.e., C:\Users\Nick\Repos\balance-exo-sim)
+motion_capture_data_path: Path to raw mocap data (download from SimTK project)
+R_exec_path: \path\to\Rscript.exe (R language executable)
+results_path: <doit_path>\results
+analysis_path: <doit_path>\analysis
+validate_path: <doit_path>\validate
+statistics_path: <doit_path>\statistics
+figures_path: <doit_path>\figures
+
+'doit' workflow
+---------------
+All steps in the simulation pipeline for this study are handled using the Python
+package 'doit', which is a task automation tool. 'Doit' keeps track of the tasks
+that have been run and which previous tasks need to be run in order to run tasks
+later in the pipeline. 
+
+The top-level 'doit' file is called 'dodo.py'; all tasks for the study eminate 
+from this file. Subject-specific tasks are defined in 'subjectXX.py' files. These
+files instantiate "tasks", which are individual blocks of simulation code that 
+'doit' will execute. The core OpenSim pipeline tasks are contained in the 
+'osimpipeline' submodule, and study-specific tasks are contained in the 
+'tasks.py'. 
+
+Tasks are executed by calling 'doit <task_name>' in PowerShell (or other
+terminal environment). Call 'doit list' to see a list of available tasks that 
+can be run. The file 'run_tasks.ps1' is a PowerShell script containing the tasks
+needed to be run to reproduce the study. The tasks are listed in order, and if 
+you run a task before running the necessary preceeding tasks, those tasks will
+be run automatically. You can force a task to run without the preceeding tasks
+by calling 'doit -s <task_name>'; this can be helpful if the 'doit' cache gets
+corrupted and you know that all preceeding tasks have been run already.
+
+Docker container
+----------------
+A Docker container that includes a Python environment and an OpenSim installation
+needed to reproduce the study results can be created from the Dockerfile located
+under the subdirectory '/docker'. The text file 'commands.txt' contains a list
+of useful commands for building the Docker image, creating a container, 
+configuring the config.yml and run_tasks.sh files, and executing the simulation
+workflow.
+
+### NOTE
+In a previous version of this project, we refered to ankle exoskeleton
 torques as 'perturbations'. The results folders are still organized using the
 terminology 'unperturbed' for normal walking simulation results, and 
 'perturbed' for results with exoskeleton torques applied. 'ankle_perturb_sim' is
--- docker/run_tasks.sh
+++ docker/run_tasks.sh
@@ -1,65 +1,65 @@
 # Generate results
 # ----------------
-# doit subject01_moco_unperturbed_guess_mesh0.04_scale0.1_reserve200
-# doit subject01_moco_unperturbed_guess_mesh0.03_scale0.5_reserve20_periodic
-# doit subject01_moco_unperturbed_guess_mesh0.02_scale1.0_reserve0_periodic
-# doit subject01_moco_unperturbed
-# doit -n 12 subject01_moco_perturbed*
+doit subject01_moco_unperturbed_guess_mesh0.04_scale0.1_reserve200
+doit subject01_moco_unperturbed_guess_mesh0.03_scale0.5_reserve20_periodic
+doit subject01_moco_unperturbed_guess_mesh0.02_scale1.0_reserve0_periodic
+doit subject01_moco_unperturbed
+doit -n 12 subject01_moco_perturbed*
 
-# doit subject02_moco_unperturbed_guess_mesh0.04_scale0.1_reserve200
-# doit subject02_moco_unperturbed_guess_mesh0.03_scale0.5_reserve20_periodic
-# doit subject02_moco_unperturbed_guess_mesh0.02_scale1.0_reserve0_periodic
-# doit subject02_moco_unperturbed
-# doit -n 12  subject02_moco_perturbed*
+doit subject02_moco_unperturbed_guess_mesh0.04_scale0.1_reserve200
+doit subject02_moco_unperturbed_guess_mesh0.03_scale0.5_reserve20_periodic
+doit subject02_moco_unperturbed_guess_mesh0.02_scale1.0_reserve0_periodic
+doit subject02_moco_unperturbed
+doit -n 12  subject02_moco_perturbed*
 
-# doit subject04_moco_unperturbed_guess_mesh0.04_scale0.1_reserve200
-# doit subject04_moco_unperturbed_guess_mesh0.03_scale0.5_reserve20_periodic
-# doit subject04_moco_unperturbed_guess_mesh0.02_scale1.0_reserve0_periodic
-# doit subject04_moco_unperturbed
-# doit -n 12 subject04_moco_perturbed*
+doit subject04_moco_unperturbed_guess_mesh0.04_scale0.1_reserve200
+doit subject04_moco_unperturbed_guess_mesh0.03_scale0.5_reserve20_periodic
+doit subject04_moco_unperturbed_guess_mesh0.02_scale1.0_reserve0_periodic
+doit subject04_moco_unperturbed
+doit -n 12 subject04_moco_perturbed*
 
-# doit subject18_moco_unperturbed_guess_mesh0.04_scale0.1_reserve200
-# doit subject18_moco_unperturbed_guess_mesh0.03_scale0.5_reserve20_periodic
-# doit subject18_moco_unperturbed_guess_mesh0.02_scale1.0_reserve0_periodic
-# doit subject18_moco_unperturbed
-# doit -n 12 subject18_moco_perturbed*
+doit subject18_moco_unperturbed_guess_mesh0.04_scale0.1_reserve200
+doit subject18_moco_unperturbed_guess_mesh0.03_scale0.5_reserve20_periodic
+doit subject18_moco_unperturbed_guess_mesh0.02_scale1.0_reserve0_periodic
+doit subject18_moco_unperturbed
+doit -n 12 subject18_moco_perturbed*
 
-# doit subject19_moco_unperturbed_guess_mesh0.04_scale0.1_reserve200
-# doit subject19_moco_unperturbed_guess_mesh0.03_scale0.5_reserve20_periodic
-# doit subject19_moco_unperturbed_guess_mesh0.02_scale1.0_reserve0_periodic
-# doit subject19_moco_unperturbed
-# doit -n 12 subject19_moco_perturbed*
+doit subject19_moco_unperturbed_guess_mesh0.04_scale0.1_reserve200
+doit subject19_moco_unperturbed_guess_mesh0.03_scale0.5_reserve20_periodic
+doit subject19_moco_unperturbed_guess_mesh0.02_scale1.0_reserve0_periodic
+doit subject19_moco_unperturbed
+doit -n 12 subject19_moco_perturbed*
 
 # Install R packages
 # ------------------
-cd statistics
-Rscript install_packages.R
-cd ..
+# cd statistics
+# Rscript install_packages.R
+# cd ..
 
 # Validate
 # --------
-doit validate_marker_errors
-doit validate_tracking_errors
-doit validate_muscle_activity
-doit validate_accelerations_versus_grfs
-doit validate_accelerations_versus_velocities
-doit compute_center_of_mass_timestepping_error 
+# doit validate_marker_errors
+# doit validate_tracking_errors
+# doit validate_muscle_activity
+# doit validate_accelerations_versus_grfs
+# doit validate_accelerations_versus_velocities
+# doit compute_center_of_mass_timestepping_error 
 
 # Statistics
 # ----------
-doit create_center_of_mass_statistics_tables
-doit create_center_of_pressure_statistics_tables
-doit run_statistics
-doit aggregate_com_statistics_results
-doit aggregate_cop_statistics_results
+# doit create_center_of_mass_statistics_tables
+# doit create_center_of_pressure_statistics_tables
+# doit run_statistics
+# doit aggregate_com_statistics_results
+# doit aggregate_cop_statistics_results
 
 # Plot results
 # ------------
-doit plot_unperturbed_results
-doit plot_center_of_mass_vector
-doit plot_instantaneous_center_of_mass
-doit plot_com_versus_cop
-doit plot_center_of_pressure_vector
-doit plot_instantaneous_center_of_pressure
-doit create_perturbation_powers_table*
-doit plot_perturbation_powers
+# doit plot_unperturbed_results
+# doit plot_center_of_mass_vector
+# doit plot_instantaneous_center_of_mass
+# doit plot_com_versus_cop
+# doit plot_center_of_pressure_vector
+# doit plot_instantaneous_center_of_pressure
+# doit create_perturbation_powers_table*
+# doit plot_perturbation_powers
